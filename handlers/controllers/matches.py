cee5d6688b227544420115de3b2498bcc12f4d93
Brokiloene <brklvn.minor@gmail.com>
2024-07-14 14:56:24 +0300

    add matches page

--- handlers/controllers/matches.py
+++ handlers/controllers/matches.py
@@ -1,4 +1,11 @@
 from .base import BaseController
 
-class MatchesHistoryController(BaseController):
-    pass
+class MatchesHistoryController(BaseController):    
+    def do_GET(self, environ, start_response):
+        status = "200 OK"
+        page = self.view.render("matches", dict())
+        self.response_headers.append(
+            ('Content-Length', str(len(page)))
+        )
+        start_response(status, self.response_headers)
+        return [bytes(page, 'utf-8')]
