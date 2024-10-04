001e202ee78e365ee87292214d0cbe050bc84fa3
Brokiloene <brklvn.minor@gmail.com>
2024-10-04 11:21:26 +0300

    make MatchDAO.fetch_all() method more efficient

--- tennis_app/src/shared/dao/match.py
+++ tennis_app/src/shared/dao/match.py
@@ -19,18 +19,24 @@ class MatchDAO(BaseDAO):
                 session.commit()
 
     def fetch_all() -> List[ReadMatchDTO]:
-        matches_dtos = []
+        matches_data = []
         with BaseDAO.new_session() as session:
-            stmt = select(MatchModel)
-            all_matches = session.execute(stmt).scalars().all()
-            for match in all_matches:
-                p1_name = session.get(PlayerModel, match.player1_id).name
-                p2_name = session.get(PlayerModel, match.player2_id).name
-                matches_dtos.append(ReadMatchDTO(
-                    player1_name=p1_name,
-                    player2_name=p2_name,
-                    score=match.score
-                ))
+            stmt = text(
+                """
+                select m.score, p1.name, p2.name 
+                from `match` m, player p1, player p2
+                where m.player1_id = p1.id and m.player2_id = p2.id
+                order by m.id
+                """
+            )
+            matches_data = session.execute(stmt)
+        matches_dtos = []
+        for score, name1, name2 in matches_data:
+            matches_dtos.append(ReadMatchDTO(
+                player1_name=name1,
+                player2_name=name2,
+                score=score
+            ))
         return matches_dtos
     
     def fetch_filtered(search_query: str):
